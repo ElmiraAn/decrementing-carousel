8e90a2c0710260991e2fde6207f204be3f2e06f8
Antelmira <elmira.abdullina.1999@mail.ru>
2022-02-17 16:59:15 +0300

    Carousel

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -1,13 +1,69 @@
 package com.epam.rd.autotasks;
 
+//import java.util.Arrays;
+
 public class CarouselRun {
 
+   int capacity;
+   public int count;
+   public static int [] decrementingCarousel;
+
+  public CarouselRun() {
+
+      capacity = DecrementingCarousel.capacity;
+      count=0;
+      for (int i=0; i<capacity; i++){
+          if (DecrementingCarousel.decrementingCarousel[i]!=0) {
+              count++;
+          }
+      }
+
+     decrementingCarousel=new int[count];
+      System.arraycopy(DecrementingCarousel.decrementingCarousel, 0, decrementingCarousel, 0, count);
+      capacity=count;
+      count=0;
+      //System.out.println(capacity);
+  }
+
     public int next() {
-       throw new UnsupportedOperationException();
+
+        //System.out.println(Arrays.toString(decrementingCarousel));
+        int summa=0;
+        for (int a:decrementingCarousel){
+            summa+=a;}
+        //System.out.println(summa);
+        if (summa==0) return (-1);
+        else{
+            if (decrementingCarousel[count]==0){
+            if (count!=(capacity-1)) {count++;}
+            else count =0;  }
+
+        int carouselDouble2 =decrementingCarousel[count];
+        decrementingCarousel[count]=decrementingCarousel[count]-1;
+
+        if (count!=(capacity-1)) {count++;}
+        else count =0;
+
+        if (decrementingCarousel[count]==0){
+           summa=0;
+           for (int a:decrementingCarousel){
+               summa+=a;}
+           if (summa!=0){
+               while (decrementingCarousel[count]==0 ) {
+                   if (count!=(capacity-1)) {count++;}
+                   else count =0;
+               }
+           }
+        }
+        return carouselDouble2;}
     }
 
     public boolean isFinished() {
-        throw new UnsupportedOperationException();
+        int summa=0;
+        for (int a:decrementingCarousel){
+            summa+=a;}
+        // DecrementingCarousel.runBe=true;
+        return summa == 0;
     }
 
 }
--- src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
@@ -1,15 +1,34 @@
 package com.epam.rd.autotasks;
 
 public class DecrementingCarousel {
-    public DecrementingCarousel(int capacity) {
 
+    public static int capacity;
+    public int count;
+    public static int counts;
+    public boolean runBe=false;
+    static int element2;
+    public static int [] decrementingCarousel;
+
+
+    public DecrementingCarousel(int capacity) {
+        DecrementingCarousel.capacity = capacity;
+        decrementingCarousel = new int [capacity];
     }
 
     public boolean addElement(int element){
-        throw new UnsupportedOperationException();
+        if (element<=0 || count==capacity || runBe) return false;
+        else {
+            element2=element;
+            decrementingCarousel[count] = element2;
+            count++;
+            counts=count;
+            return true;}
     }
 
     public CarouselRun run(){
-       throw new UnsupportedOperationException();
+        if (runBe) {return null;}
+        else {
+        runBe=true;
+        return new CarouselRun();}
     }
 }
